c2d53d00f22433f40f5f45d2876985a33ca7ce9a
Bartłomiej Organowski <bartorgan@gmail.com>
2026-03-09 08:22:36 +0000

    boze co ja robie nie wiem

--- Projekt003/index.js
+++ Projekt003/index.js
@@ -10,6 +10,8 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}));
 
+//---------------index.ejs
+
 app.post('/message', (req, res) => {
 
     db.run('INSERT INTO todo (messages, expiration_date) VALUES (?, ?)',
@@ -29,7 +31,7 @@ app.get('/', (req,res) => {
         } else {
             const data = {
                 currentTime: new Date().toLocaleString('pl-PL'),
-                messages: rows
+                messages: rows//to jest to messages w index.ejs
             };
             res.render('index', data);
         }      
@@ -55,15 +57,52 @@ app.post('/delete/:id', (req, res) => {
     res.redirect('/');
 })
 
-app.post('/edit/:id', (req, res) => {
-    const id = req.params.id;
-    res.redirect(`/edit/${id}`);
-});
-
 app.get('/edit/:id', (req, res) => {
     const id = req.params.id;
+    
     res.render('edit', { id });
 });
+
+//---------------edit.ejs
+
+app.post('/edited/:id', (req, res) => {
+
+    let mes = req.body.message;
+    let date = req.body.date;
+    let id = parseInt(req.params.id);
+
+    db.get('SELECT * FROM todo WHERE id = ?', [id], (err, row) => {
+
+        if (err) {
+            res.status(500).send('Błąd serwera');
+            return;
+        }
+
+        if (mes == "") mes = row.messages;
+        if (date == "") date = row.expiration_date;
+
+        db.run(
+            "UPDATE todo SET messages = ?, expiration_date = ? WHERE id = ?",
+            [mes, date, id],
+            function(err) {
+
+                if (err) {
+                    console.error(err.message);
+                } else {
+                    console.log(`Edytowano rekordy: ${this.changes}`);
+                }
+
+                res.redirect('/');
+            }
+        );
+    });
+});
+
+//---------------main
+
+app.get('/return', (req,res)=>{
+    res.redirect('/');
+})
 
 app.listen(PORT, ()=> {
     console.log(`Serwer dziala na http://localhost:${PORT}`);
